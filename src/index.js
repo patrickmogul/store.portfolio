17add03a17ecff8bb0cad90539301a4a92121de2
patrickmogul <patrick@easynode.pro>
2023-01-16 20:48:44 -0600

    Try #1!

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import { createRoot } from "react-dom/client";
+import { SWRConfig } from "swr";
+import "./index.css";
+import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+
+const fetcher = (url) => fetch(url).then((response) => response.json());
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <SWRConfig value={{ fetcher }}>
+    <App />
+  </SWRConfig>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
