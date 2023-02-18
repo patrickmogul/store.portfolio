86ab8c6dbd093d7944883b2e5fd799d2db7cb1bc
patrickmogul <patrick@easynode.pro>
2023-02-17 23:40:58 -0600

    g2g

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,7 +9,7 @@ export default function Navbar(props) {
   return (
     <nav className="navbar">
       <NavLink to="/" className="nav-brand">
-        Patrick's SUPER Market!
+        Patrick's SUPER Shop!
       </NavLink>
       <ul>
         <li className="nav-item">
